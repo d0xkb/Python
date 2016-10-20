945d058ead0de972c0ced4d0ccab9ab6a05a33d7
d0xkb <d0xkb@protonmail.ch>
2016-10-20 22:37:08 +0200

    time added

--- learning/rand.py
+++ learning/rand.py
@@ -1,11 +1,19 @@
 from random import randrange
+import time
+
+start = time.perf_counter()
 
 while True:
-    if (randrange(10000)) == 50:
+    if (randrange(100000)) == 50:
         break
-    print('MATCHING')
-print('DONE')
+#    print('MATCHING')
+end = time.perf_counter()
 
+t = '{:f}'.format(end-start)
+
+print('DONE IN', t, 'seconds')
+
+#print('DONE IN ', end - start, 'seconds')
 #while True:
 #    if (randrange(10000)) == 50:
 #        print('DONE')
